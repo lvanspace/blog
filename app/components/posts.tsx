743033c641d2cea7fd3be2fc073a5084d65ca095
blue@l1yu.com <blue@l1yu.com>
2024-12-07 20:30:22 +0800

    fe

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -17,7 +17,7 @@ export function BlogPosts({ count = 9 }) {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex flex-col space-y-1 mb-3"
             href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
