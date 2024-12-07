da957667ea1fdcd3fab0104bd477221745ea98d9
blue@l1yu.com <blue@l1yu.com>
2024-12-07 20:18:40 +0800

    faet

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,9 +1,8 @@
 import Link from 'next/link'
 import { formatDate, getBlogPosts } from 'app/blog/utils'
 
-export function BlogPosts() {
+export function BlogPosts({ count = 9 }) {
   let allBlogs = getBlogPosts()
-
   return (
     <div>
       {allBlogs
@@ -14,7 +13,7 @@ export function BlogPosts() {
             return -1
           }
           return 1
-        })
+        }).splice(0, count)
         .map((post) => (
           <Link
             key={post.slug}
@@ -22,15 +21,14 @@ export function BlogPosts() {
             href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[160px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              <p className="text-neutral-900 underline decoration-gray-800 dark:text-neutral-100 tracking-tight">
                 {post.metadata.title}
               </p>
             </div>
           </Link>
-        ))}
+        ))
+        }
+        { count === 9 && <Link className='mt-2' href={'/blog/'}> 📎 点击查看所有笔记</Link> }
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
-import Record from './components/record'
+import Record from "./components/record";
 
 export default function Page() {
   return (
@@ -8,14 +8,13 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         你好，来到 李宇 Blog。
       </h1>
-      <p className="mb-4">
-        {`这里存放我的笔记，想法，一切我想分享的东西。`}
-      </p>
+      <p className="mb-4">{`这里存放我的笔记，想法，一切我想分享的东西。`}</p>
       <div className="my-8">
+        <div className="font-bold text-lg pb-4">📔 最近笔记</div>
         <BlogPosts />
-
+        <div className="font-bold text-lg pb-2 pt-6">📋 最近想法</div>
         <Record />
       </div>
     </section>
-  )
+  );
 }
