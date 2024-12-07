9adf788961eed36d366108dbeb3ff1f5f052a023
blue@l1yu.com <blue@l1yu.com>
2024-12-07 20:29:05 +0800

    fat;

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       <div className="my-8">
         <div className="font-bold text-lg pb-4">📔 最近笔记</div>
         <BlogPosts />
-        <div className="font-bold text-lg pb-2 pt-6">📋 最近想法</div>
+        <div className="font-bold text-lg pb-2 pt-6">📋 最近动态</div>
         <Record />
       </div>
     </section>
