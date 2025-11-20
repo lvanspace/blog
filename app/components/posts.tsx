8b7d4e41b725bcbcee7a6d83baca5e797464b9f1
blue@l1yu.com <blue@l1yu.com>
2025-11-20 20:12:52 +0800

    feat: ...

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -20,15 +20,21 @@ export function BlogPosts({ count = 9 }) {
             className="flex flex-col space-y-1 mb-3"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 justify-between">
+
               <p className="text-neutral-900 underline dark:text-neutral-100 tracking-tight">
                 {post.metadata.title}
               </p>
+
+              <p className="dark:text-neutral-500">
+                {formatDate(post.metadata.publishedAt, false)}
+              </p>
             </div>
           </Link>
+
         ))
-        }
-        { count === 9 && <Link className='mt-2' href={'/blog/'}> 📎 点击查看所有笔记</Link> }
+      }
+      {count === 9 && <Link className='mt-2' href={'/blog/'}> 📎 点击查看所有笔记</Link>}
     </div>
   )
 }
