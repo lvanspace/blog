b4d4c32e3c6bfc5e10a6dba6010ee2338cb55b2b
blue@l1yu.com <blue@l1yu.com>
2025-11-20 17:22:03 +0800

    update: ...

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        你好，来到 李宇 Blog。
+      见字如面，李宇 Blog。
       </h1>
       <p className="mb-4">{`这里存放我的笔记，想法，一切我想分享的东西。`}</p>
       <div className="my-8">
